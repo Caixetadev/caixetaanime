a8be80e97c89490907c27c6727465827d1c496d9
Rafael Caixeta <caixetadev@gmail.com>
2021-09-02 22:54:06 -0300

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-function caixetaAnime(link){
+function caixetaAnime(link, titulo){
 fetch(`http://167.114.82.198:3333/${link}`)
 
 .then(response => response.json())
 
 .then(json => {
   console.log(json)
-  
+  document.querySelector('.container').innerHTML += `
+  <div class="containerH1">
+  <h1>${titulo}</h1>
+  </div>
+  `
   json.animes.forEach(function(anime) {
     document.querySelector('.container').innerHTML += `
     <div 
@@ -21,6 +25,7 @@ fetch(`http://167.114.82.198:3333/${link}`)
         document.querySelector(`.atrasAnime`).style.display = `block`
         caixetaId(idAnime)
       })
+      
     })
   })
 
@@ -28,10 +33,12 @@ fetch(`http://167.114.82.198:3333/${link}`)
 
 }
 
-document.querySelectorAll('.blur').forEach((me) =>{
+document.querySelectorAll('.blurAnime').forEach((me) =>{
   me.addEventListener(`click`, () => {
-    me.style.display = `none`
+    document.querySelectorAll('.blur').forEach((aa) =>{
+      aa.style.display = `none`
   })
+} )
 })
 
 function caixetaId(id) {
@@ -43,23 +50,44 @@ function caixetaId(id) {
     document.querySelector('.atrasAnime .modalAnime').innerHTML = `
     <h1 class="nomeModal">${json.nome}</h1>
     <div class="containerModal">
+    <i id="close" class="fas fa-times"></i>
     <img class="imageModal" src="${json.photo}">
     <ul class="listModal">
       <li>${json.formato}</li>
       <li>${json.genero}</li>
       <li>${json.tipoEp}</li>
       <li><b>Dia de Lançamento:</b> ${json.diaLancamento}</li>
-      <li><b>Ano:</b> ${json.ano}</li>
+      <li><b>Ano:</b> ${json.ano || 'No specifc'}</li>
     </ul>
     </div>
 
     <h4>LISTA DE EPISÓDIOS</h4>
     
     `
+    
     json.episodes.forEach(function(anime) {
       document.querySelector('.atrasAnime .modalAnime').innerHTML += `
-        <div class="modalEps">${anime.ep}</div>
+        <div class="modalEps">
+        <div
+        id="epAnime"
+        data-id="${anime.idEpisode}">
+        <a href="#">${anime.ep}</a>
+        </div>
+        </div>
       `
+    })
+
+    document.querySelector('#close').addEventListener(`click`, () => {
+      document.querySelectorAll('.blur').forEach((aa) =>{
+        aa.style.display = `none`
+        console.log(aa)
+    })
+  })
+    document.querySelectorAll(`#epAnime`).forEach(function(ep){
+      ep.addEventListener(`click`, () => {
+        const idEp = ep.getAttribute(`data-id`)
+        caixetaVideoId(idEp)
+      })
     })
   })
 }
@@ -67,13 +95,23 @@ function caixetaId(id) {
 function caixetaVideoId(idVideo) {
   fetch(`http://167.114.82.198:3333/video/${idVideo}`)
   .then(response => response.json())
-  
   .then(json => {
     console.log(json)
+    document.querySelector(`.atasVideo`).style.display = `block`
+    document.querySelector('.atasVideo .videoAnime').innerHTML = `
+    <iframe allowfullscreen
+    frameborder="0"
+    style="width: 100vw; height: 100vh; position: fixed; top: 0px; left: 0px;"
+    src="${json.video}"/>
+    `
   })
 }
+caixetaAnime('', 'Anime mais Visto')
+caixetaAnime('launchDay', 'Lançamentos do dia')
+caixetaAnime('1/naruto/caixeta', 'Tudo Sobre Naruto')
+caixetaAnime('1/dragon Ball/caixeta', 'Tudo Sobre Dragon Ball')
 
-caixetaAnime('')
+
 
 
 
